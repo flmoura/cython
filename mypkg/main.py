37e1cd8225899a408be52e31c4a511feb1225f24
Flavio <flavio.moura@gmail.com>
2021-09-07 12:38:41 -0300

    Dependência e teste com numpy adicionado

--- mypkg/main.py
+++ mypkg/main.py
@@ -1,7 +1,15 @@
+import numpy as np
 from mypkg.module3.mod3 import func_from_mod3
 
+def test_numpy():
+    a = np.arange(15).reshape(3, 5)
+    print(a)
+    print(a.shape)
+    print(a.dtype.name)
+    
 def run():
     func_from_mod3()
+    test_numpy()
 
 if __name__ == "__main__":
     run()
--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 import os
-import fnmatch
+#import fnmatch
 import sysconfig
 
 from setuptools import (
-    Extension,
     setup,
     find_packages
 )
@@ -13,7 +12,6 @@ from setuptools.command.build_py import build_py as _build_py
 EXCLUDE_FILES = [
     # 'app/main.py'
 ]
-
 
 def get_ext_paths(root_dir, exclude_files):
     """get filepaths for compilation"""
@@ -34,7 +32,6 @@ def get_ext_paths(root_dir, exclude_files):
 
 # noinspection PyPep8Naming
 class build_py(_build_py):
-
     def find_package_modules(self, package, package_dir):
         ext_suffix = sysconfig.get_config_var('EXT_SUFFIX')
         modules = super().find_package_modules(package, package_dir)
@@ -44,15 +41,6 @@ class build_py(_build_py):
                 continue
             filtered_modules.append((pkg, mod, filepath, ))
         return filtered_modules
-
-
-# sourcefiles = ["mypkg/mypkg.pyx", "mypkg/module1/mod1.py", "mypkg/module2/mod2.py", "mypkg/module3/mod3.py"]
-
-# extensions = [
-#     Extension("mypkg", sourcefiles),
-#     # Extension("mypkg", ["mypkg/*.pyx", "mypkg/module1", "mypkg/module2", "mypkg/module3"]),
-#     # Extension("*", ["*.py"], include_dirs=["mypkg/module1", "mypkg/module2", "mypkg/module3"])
-# ]
 
 # noinspection PyPep8Naming
 class build_py(_build_py):
@@ -72,17 +60,16 @@ setup(
     name = "My Package App",
     version="0.1.0",
     packages = find_packages(),
-   
+    install_requires = [
+        "numpy"
+    ],
+
     ext_modules=cythonize(
         get_ext_paths('mypkg', EXCLUDE_FILES),
         compiler_directives={'language_level': 3}
     ),
+
     cmdclass={
         'build_py': build_py
     }
-
-    # ext_modules = cythonize(extensions),
-    
-    # ext_modules = cythonize("mypkg/*.*")
-    # ext_modules = cythonize("helloworld.pyx")
 )
